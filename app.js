eca2d727304ddd59019fbbfdf0f15a5bf8abb156
Martin Galara <mgalara@gmail.com>
2023-06-09 12:56:35 -0300

    asdasdasd

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ async (ctx,{provider}) => {
 [flujoSiges,flujoImpresoraFiscal,flujoImpresoraComun,flujoDespachosCio,flujoServidor,flujoLibroIva,flujoAplicaciones])
 
 
-const asd = addKeyword(['asd'])
+const asd = addKeyword(['asdasd'])
 .addAnswer(['enviar mensaje'],
 {
     capture: true
